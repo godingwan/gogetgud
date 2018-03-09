d93ee4e8437bff425fcb43671dc8cf4ea66295a7
Josh <jsungholee@gmail.com>
2018-03-08 23:03:04 -0500

    fixed graph to actually do graph things

--- graph/socialGraph.go
+++ graph/socialGraph.go
@@ -4,20 +4,16 @@ import "fmt"
 
 type SocialGraph struct {
 	nodes []*Node
-	edges map[Node][]*Node
 }
 
 type Node struct {
-	person Person
-}
-
-type Person struct {
+	edges     []*Node
 	FirstName string
 	LastName  string
 }
 
 func (n *Node) String() string {
-	return fmt.Sprintf("%v", n.person)
+	return fmt.Sprintf("%v", n.FirstName)
 }
 
 func (g *SocialGraph) AddNode(n *Node) {
@@ -25,16 +21,61 @@ func (g *SocialGraph) AddNode(n *Node) {
 }
 
 func (g *SocialGraph) AddEdge(n1, n2 *Node) {
-	if g.edges == nil {
-		g.edges = make(map[Node][]*Node)
+	if n1.edges == nil {
+		n1.edges = make([]*Node, 0)
 	}
-	g.edges[*n1] = append(g.edges[*n1], n2)
-	g.edges[*n2] = append(g.edges[*n2], n1)
+	if n2.edges == nil {
+		n2.edges = make([]*Node, 0)
+	}
+	n1.edges = append(n1.edges, n2)
+	n2.edges = append(n2.edges, n1)
 }
 
 func (g *SocialGraph) IsRelated(mainNode, otherNode *Node) bool {
-	for _, node := range g.edges[*mainNode] {
-		if node == otherNode {
+	// need to keep track of visited nodes
+	visited := make(map[*Node]struct{}) // basically a set implementation in go
+	var empty struct{}                  // empty struct as a placeholder to make set works
+	// keep track of a queue of nodes
+	queue := []*Node{mainNode}
+	for {
+		node := queue[0]
+		visited[node] = empty // add node to set
+		queue = queue[1:]     // pop node
+		queue = getNeighbors(node, visited, queue)
+		if inNeighbor(queue, otherNode) {
+			return true
+		}
+		if len(queue) == 0 {
+			return false
+		}
+	}
+}
+
+func getNeighbors(parentNode *Node, visited map[*Node]struct{}, queue []*Node) []*Node {
+	for _, n := range parentNode.edges {
+		if _, ok := visited[n]; ok { // already visited
+			continue
+		}
+		if alreadyInQueue(queue, n) {
+			continue
+		}
+		queue = append(queue, n)
+	}
+	return queue
+}
+
+func alreadyInQueue(queue []*Node, node *Node) bool {
+	for _, n := range queue {
+		if n == node {
+			return true
+		}
+	}
+	return false
+}
+
+func inNeighbor(neighbors []*Node, searchNode *Node) bool {
+	for _, n := range neighbors {
+		if n == searchNode {
 			return true
 		}
 	}
@@ -42,16 +83,22 @@ func (g *SocialGraph) IsRelated(mainNode, otherNode *Node) bool {
 }
 
 func main() {
-	p1 := Person{"arc", "thefallen"}
-	p2 := Person{"bob", "620"}
+	var n1 Node
+	var n2 Node
+	var n3 Node
 
-	n1 := Node{p1}
-	n2 := Node{p2}
+	n1.FirstName = "arc"
+	n2.FirstName = "bob"
+	n3.FirstName = "unkani"
 
 	var twitter SocialGraph
+
 	twitter.AddNode(&n1)
 	twitter.AddNode(&n2)
-	twitter.AddEdge(&n1, &n2)
+	twitter.AddNode(&n3)
 
-	fmt.Println("Are bob and arc friends?", twitter.IsRelated(&n1, &n2))
+	twitter.AddEdge(&n1, &n3) // arc and unkani
+	twitter.AddEdge(&n2, &n3) // unkani and bob
+
+	fmt.Println("Are arc and bob connected?", twitter.IsRelated(&n1, &n2))
 }
